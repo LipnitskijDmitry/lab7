e4d6834c555cbd31ef32fd2bc240ab5dc2615f0e
LipnitskijDmitry <dmitrydust2704@gmail.com>
2021-04-14 11:24:53 +0300

    refr1-InstantMassage

---
+++ src/bsu/rfe/group7/lab7/varB/InstantMassage.java
@@ -0,0 +1,131 @@
+package bsu.rfe.group7.lab7.varB;
+
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.InetSocketAddress;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.net.UnknownHostException;
+
+import javax.swing.JOptionPane;
+import javax.swing.JTextArea;
+
+public class InstantMassage {
+
+	private String sender;
+	private MainFrame frame;
+	
+	public String getSender(){
+		return sender;
+	}
+	
+	public void setSender( String sender0 ){
+		sender = sender0;
+	}
+	
+	private void startServer(int SERVER_PORT){
+		new Thread(new Runnable() {
+
+			public void run() {
+				try {
+					
+				final ServerSocket serverSocket = new ServerSocket(SERVER_PORT);
+				
+				while (!Thread.interrupted()) {
+					final Socket socket = serverSocket.accept();
+					final DataInputStream in = new DataInputStream(
+					socket.getInputStream());
+
+					final String senderName = in.readUTF();
+					
+
+					final String message = in.readUTF();
+
+					socket.close();
+
+					final String address = 
+						((InetSocketAddress) socket
+							.getRemoteSocketAddress())
+								.getAddress()
+									.getHostAddress();
+					
+
+						frame.textAreaIncoming.append(senderName+ 
+						" (" + address + "): " + 
+						message + "\n");
+			}
+			} catch (IOException e) {
+				e.printStackTrace();
+				JOptionPane.showMessageDialog(frame,
+						"Ошибка в работе сервера", "Ошибка",
+						JOptionPane.ERROR_MESSAGE);
+			}
+		}
+	}).start();
+	}
+	
+	public void sendMessage(String senderName, String destinationAddress, 
+			int SERVER_PORT) {
+		try {
+				
+				/*/final String senderName = textFieldFrom.getText();
+				final String destinationAddress = textFieldTo.getText();
+				/*/
+			
+			final String message = frame.textAreaOutgoing.getText();
+				if (senderName.isEmpty()) {
+					JOptionPane.showMessageDialog(frame, 
+								"Введите имя отправителя", "Ошибка",
+								JOptionPane.ERROR_MESSAGE);
+					return;
+				}
+				if (destinationAddress.isEmpty()) {
+					JOptionPane.showMessageDialog(frame,
+								"Введите адрес узла-получателя", "Ошибка",
+								JOptionPane.ERROR_MESSAGE);
+					return;
+				}
+				if (message.isEmpty()) {
+					JOptionPane.showMessageDialog(frame, 
+								"Введите текст сообщения", "Ошибка",
+								JOptionPane.ERROR_MESSAGE);
+					return;
+				}
+
+				final Socket socket = new Socket(destinationAddress, SERVER_PORT);
+
+				final DataOutputStream out = new DataOutputStream(socket.getOutputStream());
+			
+				out.writeUTF(senderName);
+
+				out.writeUTF(message);
+
+				socket.close();
+
+				frame.textAreaIncoming.append("Я -> " + destinationAddress + ": "
+				+ message + "\n");
+
+				frame.textAreaOutgoing.setText("");
+			}
+		catch (UnknownHostException e) {
+				e.printStackTrace();
+				JOptionPane.showMessageDialog(frame,
+				"Не удалось отправить сообщение: узел-адресат не найден",
+					"Ошибка", JOptionPane.ERROR_MESSAGE);
+			} catch (IOException e) {
+				e.printStackTrace();
+				JOptionPane.showMessageDialog(frame,
+						"Не удалось отправить сообщение", "Ошибка",
+						JOptionPane.ERROR_MESSAGE);
+			}
+		   
+		}
+	
+	public InstantMassage(int SERVER_PORT,MainFrame frame) {
+		this.frame = frame;
+		this.startServer(SERVER_PORT);
+		
+	}
+
+}
--- src/bsu/rfe/group7/lab7/varB/MainFrame.java
+++ src/bsu/rfe/group7/lab7/varB/MainFrame.java
@@ -49,10 +49,10 @@ public class MainFrame extends JFrame {
 	private final JTextField textFieldFrom;
 	private final JTextField textFieldTo;
 	
-	private final JTextArea textAreaIncoming;
-	private final JTextArea textAreaOutgoing;
+	public final JTextArea textAreaIncoming;
+	public final JTextArea textAreaOutgoing;
 	
-	
+	private InstantMassage IM;
 	
 	public MainFrame() {
 		super(FRAME_TITLE);
@@ -97,7 +97,8 @@ public class MainFrame extends JFrame {
 		sendButton.addActionListener(new ActionListener() {
 			@Override
 			public void actionPerformed(ActionEvent e) {
-				sendMessage();
+				IM.sendMessage(textFieldFrom.getText(), textFieldTo.getText(),SERVER_PORT
+						);
 			}
 		});
 
@@ -149,99 +150,9 @@ public class MainFrame extends JFrame {
 			
 				
 				
-				new Thread(new Runnable() {
-
-				public void run() {
-					try {
-						
-					final ServerSocket serverSocket = new ServerSocket(SERVER_PORT);
-					
-					while (!Thread.interrupted()) {
-						final Socket socket = serverSocket.accept();
-						final DataInputStream in = new DataInputStream(
-						socket.getInputStream());
-
-						final String senderName = in.readUTF();
-						
-
-						final String message = in.readUTF();
-
-						socket.close();
-
-						final String address = 
-							((InetSocketAddress) socket
-								.getRemoteSocketAddress())
-									.getAddress()
-										.getHostAddress();
-						
-
-							textAreaIncoming.append(senderName+ 
-							" (" + address + "): " + 
-							message + "\n");
-				}
-				} catch (IOException e) {
-					e.printStackTrace();
-					JOptionPane.showMessageDialog(MainFrame.this,
-							"Ошибка в работе сервера", "Ошибка",
-							JOptionPane.ERROR_MESSAGE);
-				}
-			}
-		}).start();
+				IM = new InstantMassage(SERVER_PORT, this );
 	}
-	private void sendMessage() {
-		try {
-				
-				final String senderName = textFieldFrom.getText();
-				final String destinationAddress = textFieldTo.getText();
-				final String message = textAreaOutgoing.getText();
-			
-				if (senderName.isEmpty()) {
-					JOptionPane.showMessageDialog(this, 
-								"Введите имя отправителя", "Ошибка",
-								JOptionPane.ERROR_MESSAGE);
-					return;
-				}
-				if (destinationAddress.isEmpty()) {
-					JOptionPane.showMessageDialog(this,
-								"Введите адрес узла-получателя", "Ошибка",
-								JOptionPane.ERROR_MESSAGE);
-					return;
-				}
-				if (message.isEmpty()) {
-					JOptionPane.showMessageDialog(this, 
-								"Введите текст сообщения", "Ошибка",
-								JOptionPane.ERROR_MESSAGE);
-					return;
-				}
-
-				final Socket socket = 
-						new Socket(destinationAddress, SERVER_PORT);
-
-				final DataOutputStream out = 
-						new DataOutputStream(socket.getOutputStream());
-			
-				out.writeUTF(senderName);
-
-				out.writeUTF(message);
-
-				socket.close();
-
-				textAreaIncoming.append("Я -> " + destinationAddress + ": "
-				+ message + "\n");
-
-				textAreaOutgoing.setText("");
-			} catch (UnknownHostException e) {
-				e.printStackTrace();
-				JOptionPane.showMessageDialog(MainFrame.this,
-				"Не удалось отправить сообщение: узел-адресат не найден",
-					"Ошибка", JOptionPane.ERROR_MESSAGE);
-			} catch (IOException e) {
-				e.printStackTrace();
-				JOptionPane.showMessageDialog(MainFrame.this,
-						"Не удалось отправить сообщение", "Ошибка",
-						JOptionPane.ERROR_MESSAGE);
-			}
-		}
+	
 		public static void main(String[] args) {
 			SwingUtilities.invokeLater(new Runnable() {
 				
